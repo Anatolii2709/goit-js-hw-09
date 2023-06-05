695a5d1c4a7f3738110197d80c16a7667e69ed45
Anatolii Tokariev <anatoliytockarev2709@gmail.com>
2023-06-05 23:25:54 +0200

    test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const randomBodyColor = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', onBtnStart);
+stopBtn.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+  console.log(onBtnStart);
+  if (intervalId) {
+    return;
+  }
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled', 'disabled');
+
+  intervalId = setInterval(() => {
+    randomBodyColor.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+function onBtnStop() {
+  if (!intervalId) {
+    return;
+  }
+  stopBtn.setAttribute('disabled', 'disabled');
+  startBtn.removeAttribute('disabled', 'disabled');
+
+  clearInterval(intervalId);
+  intervalId = !intervalId;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,104 @@
+import Notiflix from 'notiflix';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+Notiflix.Notify.init({
+  width: '300px',
+});
+
+const input = document.querySelector('input#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const daysCounter = document.querySelector('[data-days]');
+const hoursCounter = document.querySelector('[data-hours]');
+const minutesCounter = document.querySelector('[data-minutes]');
+const secondsCounter = document.querySelector('[data-seconds]');
+const boxCounter = document.querySelector('.timer');
+
+const divFields = document.querySelectorAll('.field');
+
+boxCounter.style.display = 'flex';
+boxCounter.style.gap = '10px';
+
+divFields.forEach(divField => {
+  divField.style.display = 'flex';
+  divField.style.flexDirection = 'column';
+  divField.style.alignItems = 'center';
+});
+
+btnStart.setAttribute('disabled', 'disabled');
+input.addEventListener('input', flatpickr);
+
+let timer = new Date();
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    timer = selectedDates[0] - new Date();
+    startBtn(timer);
+  },
+};
+
+flatpickr(input, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  addLeadingZero({ days, hours, minutes, seconds });
+  if (days === 0 && hours === 0 && minutes === 0) {
+    warningTimer({ seconds });
+  }
+}
+
+function startBtn(timer) {
+  if (timer < 0) {
+    Notiflix.Notify.info('Please choose a date in the future');
+    return;
+  } else {
+    btnStart.removeAttribute('disabled', 'disabled');
+    btnStart.addEventListener('click', start);
+  }
+}
+
+function start() {
+  btnStart.setAttribute('disabled', 'disabled');
+  input.setAttribute('disabled', 'disabled');
+  let intervalId = setInterval(() => {
+    timer -= 1000;
+    if (timer <= 0) {
+      btnStart.removeAttribute('disabled', 'disabled');
+      input.removeAttribute('disabled', 'disabled');
+      clearInterval(intervalId);
+      return;
+    } else {
+      convertMs(timer);
+    }
+  }, 1000);
+}
+
+function warningTimer({ seconds }) {
+  if (seconds >= 10) {
+    return;
+  } else if (seconds >= 1) {
+    Notiflix.Notify.warning(`Залишилось : ${seconds}`);
+  } else {
+    Notiflix.Notify.success(`Час вийшов`);
+  }
+}
+
+function addLeadingZero({ days, hours, minutes, seconds }) {
+  secondsCounter.textContent = String(seconds).padStart(2, '0');
+  minutesCounter.textContent = String(minutes).padStart(2, '0');
+  hoursCounter.textContent = String(hours).padStart(2, '0');
+  daysCounter.textContent = String(days).padStart(2, '0');
+}
